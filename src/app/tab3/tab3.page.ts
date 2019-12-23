217257a4fb5dd0e959c13fb73fd2580c1b6242e0
JesusAS <jesusalmazan.2397@gmail.com>
2019-12-22 21:29:46 -0600

    Timer en tab 3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { } from 'rxjs';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,29 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  seconds: number = 11;
+  minutes: number = 0;
+  interval;
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      if(this.seconds == 0 && this.minutes > 0) {
+        this.seconds = 59;
+        this.minutes--;
+      } else if(this.seconds > 0) {
+        this.seconds--;
+      } else {
+        this.seconds = 59;
+        this.minutes = 3;
+      }
+
+      //Funcion que llama cuando termina
+      if (this.minutes == 0 && this.seconds == 0){
+        console.log('finished');
+        clearInterval(this.interval);
+      }
+
+    }, 1000);
+  }
 
 }
